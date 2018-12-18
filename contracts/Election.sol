e09c54eba8d078a9182632ec239f1105ab209790
Niklas Eblenkamp <ne024@hdm-stuttgart.com>
2018-12-18 17:12:17 +0700

    Start Commit

---
+++ contracts/Election.sol
@@ -0,0 +1,89 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint provinceId;
+        uint voteCount;
+        uint partyId;
+    }
+    // Model a Party 
+    struct Party {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+     struct Province {
+        uint id;
+        string tokens;
+    }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;    
+    mapping(uint => Party) public parties;
+    // Store Candidates Count
+    uint public candidatesCount;
+    uint public partiesCount; 
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId,     
+        uint indexed _partyId   
+    );
+
+    event eventAddedParty (
+        uint id,
+        string name,
+        uint voteCount
+    );
+
+    function Election () public {
+
+         //Initialize Parties
+        addParty("CDU");
+        addParty("SPD");
+        addParty("FDP");
+
+        addCandidate("Peter Fox", 1, 1);
+        addCandidate("Khan Humble", 1, 2);
+        addCandidate("Fred Fessler", 2,1);
+        addCandidate("Dick Smith", 2, 2);
+        addCandidate("Napol Hammer", 2, 3);  
+
+    }
+
+    function addCandidate (string _name, uint _provinceId, uint _partyOfCandidate) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, _provinceId,  0, _partyOfCandidate);
+    }
+
+     function addParty (string _name) private {
+         partiesCount++;
+         parties[partiesCount] = Party(partiesCount, _name, 0); 
+         eventAddedParty(partiesCount, _name, 0);
+     }
+       
+    function vote(uint _candidateId, uint _provinceId, uint _partyId) public {
+
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+        require(_provinceId == candidates[_candidateId].provinceId);
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+       
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        votedEvent(_candidateId, _partyId);
+    }
+}
---
+++ src/js/app.js
@@ -0,0 +1,199 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+  hasVoted: false,
+  partiesCount: 3,
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // TODO: refactor conditional
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON("Election.json", function(election) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Election = TruffleContract(election);
+      // Connect provider to interact with contract
+      App.contracts.Election.setProvider(App.web3Provider);
+
+      App.listenForEvents();
+
+      return App.render();
+    });
+  },
+
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.Election.deployed().then(function(instance) {
+      // Restart Chrome if you are unable to receive this event
+      // This is a known issue with Metamask
+      // https://github.com/MetaMask/metamask-extension/issues/2393
+      instance.votedEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        // Reload when a new vote is recorded
+        App.render();
+      });
+      instance.eventAddedParty({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        console.log("The party "+event.args.name+" was added. Id: "+event.args.id)
+        // Reload when a new vote is recorded
+        //App.render();
+      });
+    });
+  },
+
+  render: function() {
+    var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+    var loader2 = $("#loader2");
+    var content2 = $("#content2");
+    loader.show();
+    content.hide();
+
+    loader2.show();
+    content2.hide();
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+
+    
+
+    // Load contract data
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance;      
+     
+   
+     
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount) {
+      var candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
+
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
+      var partiesResults = $("#partiesResults");
+      partiesResults.empty();
+
+      var partiesSelect = $('#partiesSelect');
+      partiesSelect.empty();
+
+      
+      for (var i = 1; i <= candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
+          console.log("candidate  "+candidate);
+          var id = candidate[0];
+          var name = candidate[1];
+          var _provinceId = candidate[2];
+          var partyId = candidate[4];
+
+
+          
+          // Render candidate Result
+          
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td> <img src='imgs/" +partyId+".png' alt='' border=3 height=25 width=40></img></td><td><label class='container'> <input type='radio' value='"+id+"' name='radio2'> <span class='checkmark'></span> </label></td></tr>";
+
+          candidatesResults.append(candidateTemplate);
+          
+          
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
+        });
+      }
+      candidatesResults.append("<tr><td colspan='2'></td><td>Please choose a candidate</td><td><label class='container'> <input type='radio' checked='checked' name='radio2'> <span class='checkmark'></span> </label></td></tr>");
+      //Load Partytable
+    for (var i = 1; i <= App.partiesCount; i++) {      
+      electionInstance.parties(i).then(function(party) {
+        var id = party[0];
+        var name = party[1]; 
+        var voteCount = party[2];
+        
+        // Render party Result
+        
+        var partyTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td><img src='imgs/" +id+".png' alt='' border=3 height=25 width=40></img></td><td><label class='container'> <input type='radio' value='"+id+"' name='radio'> <span class='checkmark'></span> </label></td></tr>" 
+        partiesResults.append(partyTemplate);
+
+        // Render party ballot option
+        
+        
+      });
+    }
+    partiesResults.append("<tr><td colspan='2'></td><td>Please choose a party</td><td><label class='container'> <input type='radio' checked='checked' name='radio'> <span class='checkmark'></span> </label></td></tr>");
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted) {
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('btn btn-primary').hide();
+      }
+      $('votebtn').hide();
+      loader.hide();
+      content.show();
+      loader2.hide();
+      content2.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
+  },
+
+  castVote: function() {
+
+
+    var candidateId = $('input[name=radio2]:checked').val();
+    var partyId = $('input[name=radio]:checked').val();
+    console.log("ate: "+candidateId+" and the Party:"+partyId)  ;
+    if(candidateId ==  "on"){
+      alert("Please choose a candidate and try again");
+      return;
+    } 
+    if(partyId == "on"){
+      alert("Please choose a party and try again");  
+      return;
+    } 
+       
+    App.contracts.Election.deployed().then(function(instance) {   //function vote(uint _candidateId, uint _provinceId, uint _partyId) public {
+    //console.log("You are schure you want to vote for Candidate: "+candidateId+++" and the Party:"+partyId++)  
+      return instance.vote(candidateId, 1, partyId,{ from: App.account });
+    }).then(function(result) {
+
+      
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show(); 
+      $("#content2").hide();
+      $("#loader2").show(); 
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
